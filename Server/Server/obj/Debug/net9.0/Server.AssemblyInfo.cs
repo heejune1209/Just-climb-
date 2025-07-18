c9f528f68d09832ce36c3f69359db24da982db4a
heejune <heejune1209@gmail.com>
2025-07-18 17:51:13 +0900

    sdfgb

--- Server/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3700211d3291ef6c50a6abb6fe00a65cf9ed7d8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d587549f2636f0414bd8d8cbd119266cc630e089")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
